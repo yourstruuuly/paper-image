9a0f5b0199f59aec49cae03ee94ea334ed1926dc
yourstruuuly <1>
2026-04-14 05:43:27 +0100

    fix noob bug

--- entry.sh
+++ entry.sh
@@ -6,7 +6,7 @@ cd $GAMEDIR
 
 # If the paper binary is missing, download it and accept EULA
 if [ ! -f "paper.jar" ]; then
-    // A download link must be provided otherwise complain
+    # A download link must be provided otherwise complain
     if [ -z "$PAPER_DOWNLOAD" ]; then
         printf "PAPER_DOWNLOAD variable unset or empty. This must be a direct download link to a paper jar file.\nHead to https://papermc.io/downloads/paper"
         exit 1
